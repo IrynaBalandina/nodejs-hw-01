e02aa5ecb40136c79ab0390a74ddc0a0369603b3
Iryna <mefodievna2908@gmail.com>
2024-12-01 21:34:07 +0200

    finish

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,8 +1,12 @@
 import {createFakeContact} from "../utils/createFakeContact.js";
-
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
 
 const generateContacts = async (number) => {
-    const newSongs = Array(number).fill(0).map(createFakeContact);
+    const contactList =await readContacts();
+    const newContacts = Array(number).fill(0).map(createFakeContact);
+    const newList = [...contactList, ...newContacts];
+    await writeContacts(newList);
 };
 
 generateContacts(5);
